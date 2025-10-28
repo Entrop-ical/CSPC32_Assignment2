c0b6c0bb71279b39966e6ce21a3bf66ecab493bc
Pradeeshwaran M <pradeeshwaran2006@gmail.com>
2025-10-28 19:18:12 +0530

    Update platform.h

--- platform.h
+++ platform.h
@@ -8,14 +8,11 @@ typedef struct platform {
 } platform;
 
 platform* createplatform();
-int addpost(char *user,char *cap);
-int deletepost(int n);
-post* viewpost(int n);
-post* currpost();
-int addcomment(char *user,char *text);
-int deletecomment(int n);
-comment* viewcomments();
-int addreply(char *user,char *text,int n);
-int deletereply(int n,int m);
+void addpost(char *user,char *cap);
+void viewposts();
+void addcomment(char *user,char *text,int n);
+void addreply(char *user,char *text,int pn,int cn);
+void viewcomments(int n);
+void deletepost(int n);
 
 #endif
